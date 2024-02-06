fd9c2bb34ec9587343f733ec2384b1d0d2d12f02
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-06 15:37:20 +0800

    alvistack/lxml-5.3.0
        git clean -xdf
        tar zcvf ../python-lxml_5.3.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-lxml.spec ../python-lxml_5.3.0-1.spec
        cp ../python*-lxml*5.3.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/lxml-lxml-5.3.0/
        rm -rf ../python*-lxml*5.3.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ def setup_extra_options():
 
 setup(
     name = "lxml",
-    version = lxml_version,
+    version = "5.3.0",
     author="lxml dev team",
     author_email="lxml-dev@lxml.de",
     maintainer="lxml dev team",
